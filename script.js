492693200992a7586f5ab8efc1102b162eea5049
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-02 15:44:02 +0100

    started doing if to change colors

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let blue = 100;
 
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
+const changeColor = (e) => {
+    if(e.keyCode == 38) {
 
+    } else if(e.keyCode == 40) {
+        
+    }
+}
 
 window.addEventListener("keydown", changeColor)
