56bd36926d7d62984c4145e04e2fc1539f91ec88
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-02 15:54:54 +0100

    added switch

--- script.js
+++ script.js
@@ -4,17 +4,32 @@ let blue = 100;
 
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
+// IF
+
+// const changeColor = (e) => {
+//     if (e.keyCode == 38 && red <= 255) {
+//         red += 3
+//         blue += 3
+//         green += 3
+//         document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+//     } else if (e.keyCode == 40 && red >= 0) {
+//         red -= 3
+//         blue -= 3
+//         green -= 3
+//         document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+//     }
+// }
+
+
+// SWITCH
 const changeColor = (e) => {
-    if (e.keyCode == 38 && red <= 255) {
-        red += 3
-        blue += 3
-        green += 3
-        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-    } else if (e.keyCode == 40 && red >= 0) {
-        red -= 3
-        blue -= 3
-        green -= 3
-        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    switch (e.keyCode) {
+        case 38:
+            document.body.style.backgroundColor = `rgb(${red+=3}, ${green+=3}, ${blue+=3})`
+            break;
+        case 40:
+            document.body.style.backgroundColor = `rgb(${red-=3}, ${green-=3}, ${blue-=3})`
+            break;
     }
 }
 
