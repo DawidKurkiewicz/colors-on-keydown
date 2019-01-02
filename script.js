9dcb2b1c56b79c58677a65aeb7c864397fb65004
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-02 15:50:28 +0100

    refactor

--- script.js
+++ script.js
@@ -5,21 +5,16 @@ let blue = 100;
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
 const changeColor = (e) => {
-    if (e.keyCode == 38) {
-        if (red <= 255) {
-            red += 3
-            blue += 3
-            green += 3
-            document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-        }
-    } else if (e.keyCode == 40) {
-        if (red >= 0) {
-            red -= 3
-            blue -= 3
-            green -= 3
-            document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-
-        }
+    if (e.keyCode == 38 && red <= 255) {
+        red += 3
+        blue += 3
+        green += 3
+        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    } else if (e.keyCode == 40 && red >= 0) {
+        red -= 3
+        blue -= 3
+        green -= 3
+        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
     }
 }
 
