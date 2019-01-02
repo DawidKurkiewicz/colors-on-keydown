082170efae6c487d0dcda58d7c93d1621f5e61a0
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-02 15:58:15 +0100

    added min and max in switch

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 const changeColor = (e) => {
     switch (e.keyCode) {
         case 38:
-            document.body.style.backgroundColor = `rgb(${red+=3}, ${green+=3}, ${blue+=3})`
+            document.body.style.backgroundColor = `rgb(${red <= 255 ? red+=3 : red}, ${green <= 255 ? green+=3 : green}, ${blue <= 255 ? blue+=3 : blue})`
             break;
         case 40:
-            document.body.style.backgroundColor = `rgb(${red-=3}, ${green-=3}, ${blue-=3})`
+            document.body.style.backgroundColor = `rgb(${red >= 0 ? red-=3 : red}, ${green >= 0 ? green-=3 : 0 }, ${blue >= 0 ? blue-=3 : blue})`
             break;
     }
 }
